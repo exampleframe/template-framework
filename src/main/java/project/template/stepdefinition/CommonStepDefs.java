93d51503669792b61256124e151293ec67a75fec
mdakulin@gmail.com <291192213MKate>
2018-06-13 15:51:30 +0700

    Selenide

--- src/main/java/project/template/stepdefinition/CommonStepDefs.java
+++ src/main/java/project/template/stepdefinition/CommonStepDefs.java
@@ -1,11 +1,6 @@
 package project.template.stepdefinition;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.ru.Когда;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CommonStepDefs {
 
--- src/main/java/project/template/stepdefinition/Init.java
+++ src/main/java/project/template/stepdefinition/Init.java
@@ -1,13 +1,12 @@
 package project.template.stepdefinition;
 
+import com.codeborne.selenide.Configuration;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 final public class  Init {
     private static WebDriver driver;
@@ -18,12 +17,13 @@ final public class  Init {
 
     @Before
     public void start(){
-        ChromeDriverManager.getInstance().setup();
-        driver = new ChromeDriver();
+        Configuration.browser = "chrome";
+        driver = getWebDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+        open("https://mail.yandex.ru");
     }
 
     @After
