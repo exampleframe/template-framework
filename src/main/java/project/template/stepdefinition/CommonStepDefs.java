0337414bb3e19b96178050d5e84579f7a8fcea09
mdakulin@gmail.com <291192213MKate>
2018-06-13 14:11:29 +0700

    Project

---
+++ src/main/java/project/template/stepdefinition/CommonStepDefs.java
@@ -0,0 +1,11 @@
+package project.template.stepdefinition;
+
+import cucumber.api.java.ru.Когда;
+
+public class CommonStepDefs {
+
+    @Когда("^пользователь переходит на страницу авторизации$")
+    public void пользовательПереходитНаСтраницуАвторизации() {
+
+    }
+}
