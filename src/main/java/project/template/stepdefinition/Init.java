d3f7861935ee85ddb3e789771f97db6dc900a4b9
mdakulin@gmail.com <291192213MKate>
2018-06-13 17:24:44 +0700

    Init

--- src/main/java/project/template/stepdefinition/Init.java
+++ src/main/java/project/template/stepdefinition/Init.java
@@ -1,11 +1,12 @@
 package project.template.stepdefinition;
 
-import com.codeborne.selenide.Configuration;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 final public class  Init {
@@ -13,13 +14,15 @@ final public class  Init {
 
     @Before
     public void start(){
-        Configuration.browser = "chrome";
+        setProperty();
+
         driver = getWebDriver();
+
+        Long timeout = Long.parseLong(System.getProperty("timeout"));
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-        open("https://mail.yandex.ru");
+        driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(timeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(timeout, TimeUnit.SECONDS);
     }
 
     @After
@@ -29,5 +32,14 @@ final public class  Init {
         }
     }
 
+    private static void setProperty() {
+        Properties props = new Properties();
+        try {
+            props.load(new FileReader("./src/test/config/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        props.stringPropertyNames().stream().forEach(key -> System.setProperty(key, props.getProperty(key)));
+    }
 
 }
